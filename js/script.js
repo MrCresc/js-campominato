54f75c53b264f862f6b823629a1c4e4ff7ab0be1
Crescenzo De Martino <cresckamina@gmail.com>
2020-06-03 17:19:20 +0200

    Campo minato funzionante al 100%

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var arrayBombe = [];
 while (livelloDifficolta !== 0 && livelloDifficolta !== 1 && livelloDifficolta !== 2) {
   livelloDifficolta = parseInt(prompt('Valore immesso errato! Scegli il livello di difficoltà. Digita \"0\" per difficoltà facile, digita \"1\" per difficoltà intermedia oppure digita \"2\" per difficoltà difficile'));
 }
-
+// Imposto numeri random e numeri possibili per l'utente a seconda della difficoltà
 if (livelloDifficolta === 0) {
   arrayBombe = numeriRandomNonRipetuti(16,100)
   var numeriPossibili = 84
@@ -17,15 +17,16 @@ if (livelloDifficolta === 0) {
   arrayBombe = numeriRandomNonRipetuti(16,50)
   var numeriPossibili = 34
 }
-
+// Cheat attivabile per sapere i numeri dove sono posizionate le mine
 console.log(arrayBombe)
+// Variabili finali
 var arrayUtente = []
-var numeroUtente = 0
+var numeroUtente
 var i = 0;
-
-while (arrayBombe.includes(numeroUtente) || i < numeriPossibili) {
+// Verifico che il numero non sia una bomba e che ci siano ancora numeri possibili
+while (!arrayBombe.includes(numeroUtente) && i-1 < numeriPossibili) {
   numeroUtente = parseInt(prompt('Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
-
+  // Verifico che il valore immesso sia corretto o che non sia già stato scelto precedentemente
   while (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente) || arrayUtente.includes(numeroUtente)) {
     if (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente)) {
       numeroUtente = parseInt(prompt('Valore immesso errato! Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
@@ -33,39 +34,15 @@ while (arrayBombe.includes(numeroUtente) || i < numeriPossibili) {
       numeroUtente = parseInt(prompt('Valore immesso già scelto precedentemente! Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
     }
   }
-
   arrayUtente.push(numeroUtente)
   i++;
-  console.log(arrayUtente)
 }
-
-alert('hai perso')
-
-
-
-
-// var i = 0;
-// var numeroUtente = parseInt(prompt('Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
-// while (numeroUtente < 1 || isNaN(numeroUtente) numeroUtente > 100) {
-//   numeroUtente = parseInt(prompt('Valore immesso errato! Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
-// }
-//
-// while (i < numeriPossibili && !arrayBombe.includes(numeroUtente)) {
-//   alert('BRAVO!!!')
-//   var numeroUtente = parseInt(prompt('Inserire un altro numero dove pensi non ci sia una bomba'));
-//
-//
-//   i++
-// }
-// if (i < numeriPossibili) {
-//   alert('HAI PERSO!!! Hai totalizzato un punteggio di ' + i + ' su ' + numeriPossibili)
-// } else {
-//   alert('HAI VINTO!!! Hai totalizzato un punteggio di ' + i + ' su ' + numeriPossibili)
-// }
-//
-
-
-
+// Mostro l'esito all'utente in caso di vittoria o sconfitta
+if (i < numeriPossibili) {
+  alert('Che peccato hai perso!!! Hai totalizzato ' + i + ' su ' + numeriPossibili)
+} else {
+  alert('HAI VINTO!!! Hai totalizzato ' + i + ' su ' + numeriPossibili)
+}
 
 // Funzione che calcola numeri non ripetuti
 function numeriRandomNonRipetuti(quantitaNumeri, limiteNumeri) {
@@ -79,13 +56,4 @@ function numeriRandomNonRipetuti(quantitaNumeri, limiteNumeri) {
   }
   arrayBombe.sort()
   return arrayBombe;
-}
-
-// Funzione che controlla se un numero immesso dall'utente è già stato inserito in precedenza
-function numeriUtenteRipetuti(numero,arrayUtente) {
-  var arrayUtenteRipetuti = false;
-  if (arrayUtente.includes(numero)) {
-    arrayUtenteRipetuti = true;
-  }
-  return arrayUtenteRipetuti;
 }
