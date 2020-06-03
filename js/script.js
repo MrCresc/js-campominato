932d49fbd5c48f012cf96b666b9acd3e74816de8
Crescenzo De Martino <cresckamina@gmail.com>
2020-06-03 17:30:51 +0200

    Corretto errore grafico "inserisci un numero da 1 a 100/80/50"

--- js/script.js
+++ js/script.js
@@ -10,12 +10,15 @@ while (livelloDifficolta !== 0 && livelloDifficolta !== 1 && livelloDifficolta !
 if (livelloDifficolta === 0) {
   arrayBombe = numeriRandomNonRipetuti(16,100)
   var numeriPossibili = 84
+  var limiteRange = 100;
 } else if (livelloDifficolta === 1) {
   arrayBombe = numeriRandomNonRipetuti(16,80)
   var numeriPossibili = 64
+  var limiteRange = 80;
 } else {
   arrayBombe = numeriRandomNonRipetuti(16,50)
   var numeriPossibili = 34
+  var limiteRange = 50;
 }
 // Cheat attivabile per sapere i numeri dove sono posizionate le mine
 console.log(arrayBombe)
@@ -25,13 +28,13 @@ var numeroUtente
 var i = 0;
 // Verifico che il numero non sia una bomba e che ci siano ancora numeri possibili
 while (!arrayBombe.includes(numeroUtente) && i-1 < numeriPossibili) {
-  numeroUtente = parseInt(prompt('Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
+  numeroUtente = parseInt(prompt('Inserire un numero da 1 a ' + limiteRange + ' dove pensi non ci sia una bomba'));
   // Verifico che il valore immesso sia corretto o che non sia già stato scelto precedentemente
-  while (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente) || arrayUtente.includes(numeroUtente)) {
-    if (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente)) {
-      numeroUtente = parseInt(prompt('Valore immesso errato! Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
+  while (numeroUtente < 1 || numeroUtente > limiteRange || isNaN(numeroUtente) || arrayUtente.includes(numeroUtente)) {
+    if (numeroUtente < 1 || numeroUtente > limiteRange || isNaN(numeroUtente)) {
+      numeroUtente = parseInt(prompt('Valore immesso errato! Inserire un numero da 1 a ' + limiteRange + ' dove pensi non ci sia una bomba'));
     } else {
-      numeroUtente = parseInt(prompt('Valore immesso già scelto precedentemente! Inserire un numero da 1 a 100 dove pensi non ci sia una bomba'));
+      numeroUtente = parseInt(prompt('Valore immesso già scelto precedentemente! Inserire un numero da 1 a ' + limiteRange + ' dove pensi non ci sia una bomba'));
     }
   }
   arrayUtente.push(numeroUtente)
